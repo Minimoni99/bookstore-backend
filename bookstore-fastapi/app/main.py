fc6dbedf8cefea62af7516a55184dd006cb59aea
Minimoni99 <bethelonyedum@gmail.com>
2026-09-10 14:56:23 +0100

    Add temporary admin reset endpoint

--- bookstore-fastapi/app/main.py
+++ bookstore-fastapi/app/main.py
@@ -6,7 +6,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.staticfiles import StaticFiles
 
-from . import routes_auth, routes_books, routes_checkout, routes_webhooks, routes_admin, routes_downloads, routes_settings, routes_leads
+from . import routes_auth, routes_books, routes_checkout, routes_webhooks, routes_admin, routes_downloads, routes_settings, routes_leads, routes_bootstrap
 
 app = FastAPI(title="Ebook Store API")
 
@@ -34,6 +34,7 @@ app.include_router(routes_downloads.router)
 app.include_router(routes_settings.router)
 app.include_router(routes_leads.router)
 app.include_router(routes_leads.admin_router)
+app.include_router(routes_bootstrap.router)
 
 
 @app.get("/health")
